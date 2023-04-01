e047514a5499035eda8e2a26f330dcb3df942501
Кирилл Осинцев <osintsev.2000@inbox.ru>
2023-04-01 09:19:31 +0500

    added review link

--- landing-sections/MainSection/index.tsx
+++ landing-sections/MainSection/index.tsx
@@ -1,5 +1,6 @@
 import Button from '@/components/Button';
 import Text from '@/components/Text';
+import Link from 'next/link';
 import { useRouter } from 'next/router';
 import Props from './MainSection.props';
 
@@ -16,12 +17,19 @@ const MainSection: React.FC<Props> = ({ className = '', ...props }) => {
 			</Button>
 			<Button
 				variant='text'
-				className='mb-4'
+				className='mb-2'
 				onClick={() => router.push('https://forms.yandex.ru/cloud/63c41a9bc417f367518432cc/')}
 			>
 				Стать куратором
 			</Button>
-			<video className='rounded-2xl mb-[50px] lg:mb-24' poster='/assets/demo_preview.png' controls muted autoPlay>
+			<Link
+				href='https://forms.yandex.ru/cloud/6424203969387206b8231c32/'
+				target='_blank'
+				className='text-primary font-semibold ml-4'
+			>
+				Оставьте отзыв о нас
+			</Link>
+			<video className='rounded-2xl mb-[50px] lg:mb-24 mt-4' poster='/assets/demo_preview.png' controls muted autoPlay>
 				<source src='/assets/demo.mp4' type='video/mp4' />
 			</video>
 		</section>
